c6741c566f1624cbfe64faa6647df4f886849f4b
Felipe2009 <felipe.morassutti@hotmail.com>
2020-08-28 12:55:25 -0300

    arrumando detalhes finais

---
+++ frontend/.history/src/pages/Estoque/index_20200827205753.jsx
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from 'react';
+import './styles.css';
+import logoImg from '../../assets/gots.png'
+import figurab from '../../assets/figurab.png'
+import figurabb from '../../assets/figurabb.png'
+import figurao from '../../assets/figurao.png'
+import figuraoo from '../../assets/figuraoo.png'
+import figuraab from '../../assets/figuraab.png'
+import figuraabab from '../../assets/figuraabab.png'
+import figuraa from '../../assets/figuraa.png'
+import figuraaa from '../../assets/figuraaa.png'
+import api from '../../services/api';
+import { Link, useHistory } from 'react-router-dom';
+
+
+export default function Estoque() {
+    const [estoques, setEstoques] = useState([]);
+    const imagens = {
+        'A+': figuraa,
+        'A-': figuraaa,
+        'B+': figurabb,
+        'B-': figurab,
+        'AB+': figuraab,
+        'AB-': figuraabab,
+        'O+': figuraoo,
+        'O-': figurao
+    }
+
+
+    const data = [estoques.map(estoque => (<li className={estoque.tipo}>
+        <ul className="imagem">
+            <strong>Quantidade Mínima:</strong>
+            <p>20 bolsas</p>
+
+            <strong>Quantidade Disponível em Bolsas:</strong>
+
+            <input className="xxx" type="text" value={estoque.quantidade}></input>
+            <p><img className={estoque.tipo} src={imagens[estoque.tipo]}></img></p>
+
+        </ul>
+
+    </li>
+
+
+    ))]
+    console.log(data);
+
+    const funcionarioEmail = localStorage.getItem('funcionarioEmail');
+
+
+    useEffect(() => {
+        api.get('estoque').then(response => {
+            setEstoques(response.data);
+        })
+    }, [funcionarioEmail]);
+
+
+    return (
+        <div className="estoque-container">
+            <header>
+                <img classname="imagem" src={logoImg}></img>
+                <Link className="aaaaa" to="/cadastroHistorico">Cadastrar Doação</Link>
+                <Link className="voltaagendar" to="/agendar"> Agendar Doação</Link>
+            </header>
+            <Link className="listagema" to="/lista">Lista de doadores</Link>
+
+            <h1 className="textao">Estoque</h1>
+
+
+             <br></br> 
+            <ul >
+                {data}
+
+            </ul>
+
+            {/* <br></br> */}
+
+
+
+        </div>
+
+    )
+}
---
+++ frontend/.history/src/pages/ListaAgendamento/index_20200827214858.jsx
@@ -0,0 +1,84 @@
+import React, { useState, useEffect } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import './styles.css';
+import logoImg from '../../assets/gots.png'
+import api from '../../services/api';
+import MUIDataTable from "mui-datatables";
+import {FiTrash} from 'react-icons/fi'
+
+export default function ListaAgendamento() {
+    const [agenda, setAgenda] = useState([]);
+    const history = useHistory();
+    const $ = require('jquery');
+    $.DataTable = require('datatables.net');
+  
+    const funcionarioEmail = localStorage.getItem('funcionarioEmail');
+    const columns = ["name", "dia", "horario", "tipo", "sexo"];
+
+    const data = [agenda.map(agendar => (<tr key={agendar.name}>
+
+        <td>{agendar.name}</td>
+        <td>{agendar.dia}</td>
+        <td>{agendar.horario}</td>
+        <td>{agendar.tipo}</td>
+        <td>{agendar.sexo}</td>
+
+        
+        <FiTrash onPress={() => handleDeleteAgendamento(agendar.rg)} type="button"> </FiTrash> 
+    </tr>
+    ))]
+    const options = {
+        filter: true,
+        search: true,
+    };
+    // use effect serve para disparar uma função em um determinado momento do componente
+    useEffect(() => {
+        api.get('listaagendamento').then(response => {
+            setAgenda(response.data);
+        })
+    }, [funcionarioEmail]);
+
+    async function handleDeleteAgendamento(rg) {
+        try {
+            await api.delete(`agendar/${rg}`, {
+
+            });
+            setAgenda(agenda.filter(agendar => agendar.rg != rg));
+        }
+        catch (err) {
+            alert("Erro ao deletar agendamento")
+        }
+    }
+
+       return (
+        <div className="lista-container">
+            <header>
+                <img src={logoImg} />
+                <Link className="botaoo" to="/cadastroDoador">Cadastrar Doador</Link>
+                <Link className="voltar" to="/"> Voltar para home</Link>
+
+            </header>
+
+            <h1 className="textao">Lista de Agendamento</h1>
+            <br></br>
+
+            <Link className="verificarestoquelistaagendamento" to="/estoque"> Verificar estoque</Link>
+            <br></br>
+
+            <Link className="agendamentodoacao" to="/agendar"> Agendar doação</Link>
+            <br></br>
+            <MUIDataTable className="cell-border"
+                //data = {data}
+                data={agenda}
+                columns={columns}
+                options={options}
+
+            >
+
+            </MUIDataTable>
+
+
+        </div>
+
+    )
+}
